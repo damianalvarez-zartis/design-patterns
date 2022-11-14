41fe34c3ce2d812a8918391212c5d4221bd4493f
Damian Alvarez @ Evotix <damian.alvarez@evotix.com>
2022-11-14 09:51:58 +0100

    Email classic constructor

---
+++ EmailBuilder/Email.cs
@@ -0,0 +1,22 @@
+// Classic email class with classic constructor
+public class Email
+{
+    public string From { get; set; }
+    public string To { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+    public bool IsHtmlBody { get; set; }
+    public string? Cc { get; set; }
+    public string? Bcc { get; set; }
+
+    public Email(string from, string to, string subject, string body, bool isHtmlBody = false, string? cc = null, string? bcc = null)
+    {
+        From = from;
+        To = to;
+        Subject = subject;
+        Body = body;
+        IsHtmlBody = isHtmlBody;
+        Cc = cc;
+        Bcc = bcc;
+    }
+}
--- EmailBuilder/Program.cs
+++ EmailBuilder/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Guille!");
+using Newtonsoft.Json;
+
+var loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+var email = new Email("target@mail.com", "sender@mail.com", "Hello there!", loremIpsum);
+
+Console.WriteLine(JsonConvert.SerializeObject(email));
