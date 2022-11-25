3da71148618e1a3ce00ac1dea67ab57c952db50d
Damian Alvarez @ Evotix <damian.alvarez@evotix.com>
2022-11-25 17:56:45 +0100

    base classes for abstract factory

---
+++ AbstractFactory/Models/Price.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory.Models
+{
+    public class Price
+    {
+        public decimal BasePrice { get; set; }
+        public decimal VAT { get; set; }
+
+        public decimal Total()
+        {
+            //TODO: for now
+            return BasePrice;
+        }
+    }
+}
